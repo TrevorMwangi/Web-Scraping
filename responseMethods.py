d02964cc52fa356cbfb5f1bc71d52194574e4a34
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-03-25 22:58:12 +0300

    Update responseMethods.py
    tests of response method

--- responseMethods.py
+++ responseMethods.py
@@ -1,8 +1,10 @@
 import requests
 
-response = requests.get("https://api.github.com/users/TrevorMwangi")
+response = requests.get("https://api.github.com/")
 print(response.url)
-print(response.history)
+print(response.headers)
+print(response.elapsed)
+
 
 # response.status_code returns the status code from the headers 
 # itself, to check if successful
