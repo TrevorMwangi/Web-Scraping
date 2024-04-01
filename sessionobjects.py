4f529aef1f7ee41528e920931b2e60937ff8feda
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-04-01 23:57:29 +0300

    Update sessionobjects.py
    notes

--- sessionobjects.py
+++ sessionobjects.py
@@ -12,3 +12,6 @@ r = s.get('https://httpbin.org/cookies')
 
 # check if cookie is still set 
 print(r.text) 
+
+''' illustrating the use of session objects by setting a cookie to a URL 
+    and then making a request again to check if the cookie is set '''
